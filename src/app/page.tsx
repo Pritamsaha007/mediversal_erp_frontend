09fb00ab95d3a5d84a66a8922f9f2123e3143ab5
Pritamsaha007 <pritam.saha@mediversal.in>
2025-04-24 15:34:46 +0530

    fix: add dynamic export for Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { Suspense } from "react";
 import LoginScreen from "../app/login/page";
 import { Toaster } from "react-hot-toast";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
