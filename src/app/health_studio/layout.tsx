ba5f73299cf50ddceff55be8a42315d576a02012
Pritamsaha007 <pritam.saha@mediversal.in>
2025-04-25 15:08:57 +0530

    refactor: remove ProtectedRoute from layout components and update logo import path in MediversalMaatri

--- src/app/health_studio/layout.tsx
+++
@@ -1,8 +0,0 @@
-import ProtectedRoute from "../components/protectedRoute";
-export default function HealthStudio({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-}
